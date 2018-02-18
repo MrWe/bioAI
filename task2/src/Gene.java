ca68554f4655f1bbdfd8da4da0219350081d86a2
agnete <agnetedjupvik@gmail.com>
2018-02-18 14:28:47 +0100

    Create gene

---
+++ task2/src/Gene.java
@@ -0,0 +1,18 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Gene {
+    ArrayList gene;
+
+    public Gene{
+
+    }
+
+    public void setGene(ArrayList gene){
+        this.gene = gene;
+    }
+
+    public ArrayList getGene(){
+        return gene;
+    }
+}
