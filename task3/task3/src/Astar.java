972689c3ad42e1f49e33ea0d1139df0536a0a50e
mrwe <erikwik@stud.ntnu.no>
2018-04-26 10:18:30 +0200

    Removed 3d walker

--- task3/task3/src/Astar.java
+++ task3/task3/src/Astar.java
@@ -11,78 +11,8 @@ public class Astar {
     AstarSolution bestSolution;
 
     public Astar(int optimalValue) {
-        //Randrun(optimalValue);
-        run3DWalker(optimalValue);
-    }
+        Randrun(optimalValue);
 
-    private void run3DWalker(int optimalValue) {
-
-        int bestScore = Integer.MAX_VALUE;
-        ArrayList<AstarSolution> queue = new ArrayList<>();
-
-        for (int i = 0; i < 5; i++) {
-            queue.add(new AstarSolution(new Gene(ImportJobs.numJobs, ImportJobs.numMachines)));
-        }
-
-        for (int i = 0; i < 500; i++) {
-
-            for (int j = queue.size()-1; j >= 0 ; j--) {
-
-                ArrayList<AstarSolution> neighbours = get3DWalkerNeighbours(queue.remove(j));
-
-                queue.addAll(neighbours);
-
-                for(AstarSolution as : neighbours){
-                    if(as.getScore() < bestScore){
-                        bestScore = as.getScore();
-                        bestMachines = as.getMachines();
-                        System.out.println("Fant ny best! " + bestScore + " Optimal: " + optimalValue);
-                    }
-                }
-
-            }
-            if(queue.size() > 500){
-                queue = pruneQueue(queue);
-            }
-
-        }
-    }
-
-    private ArrayList<AstarSolution> pruneQueue(ArrayList<AstarSolution> queue){
-        PriorityQueue<AstarSolution> priQueue = new PriorityQueue<>(queue);
-        ArrayList<AstarSolution> newQueue = new ArrayList<>();
-
-        for (int i = 0; i < 100; i++) {
-            newQueue.add(priQueue.poll());
-        }
-        return newQueue;
-    }
-
-    private ArrayList<AstarSolution> get3DWalkerNeighbours(AstarSolution current) {
-        PriorityQueue<AstarSolution> neighbours = new PriorityQueue<>();
-        for (int i = 0; i < 500; i++) {
-            SplittableRandom r = new SplittableRandom();
-            ArrayList<Integer> sequence = new ArrayList<>(current.getGene().getQueue());
-
-            int iter = r.nextInt(3)+1;
-
-            for (int j = 0; j < iter; j++) {
-
-                int r1 = r.nextInt(sequence.size());
-                int r2 = r.nextInt(sequence.size());
-
-                Collections.swap(sequence, r1, r2);
-            }
-
-            neighbours.add(new AstarSolution(new Gene(sequence)));
-        }
-
-        ArrayList<AstarSolution> returnNeighbours = new ArrayList<>();
-
-        for (int i = 0; i < 50; i++) {
-            returnNeighbours.add(neighbours.poll());
-        }
-        return returnNeighbours;
     }
 
     public Astar(Gene start){
